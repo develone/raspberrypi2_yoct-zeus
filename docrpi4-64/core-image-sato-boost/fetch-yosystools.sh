0eaed1c517e94c5a599e3ddde4bdaaf7f72db1c2
Ed Vidal Jr <devel@mypi3-15.attlocal.net>
2019-12-13 18:08:17 -0700

    currently missing tcl.h

---
+++ docrpi4-64/core-image-sato-boost/fetch-yosystools.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir repo-yosys
+cd repo-yosys 
+git clone https://github.com/cliffordwolf/icestorm.git
+git clone  https://github.com/develone/arachne-pnr.git
+git clone https://github.com/develone/nextpnr.git
+git clone  https://github.com/develone/yosys.git
